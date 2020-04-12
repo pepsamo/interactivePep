147c5268575c03e5d3a2e61ddbdccb3d5b57edda
pepsamo <giuseppegiovanni.amoruso@gmail.com>
2020-04-12 12:15:13 +0200

    Create header component

---
+++ src/app/my-header/my-header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line: component-selector
+  selector: 'my-header',
+  templateUrl: './my-header.component.html',
+  styleUrls: ['./my-header.component.scss']
+})
+export class MyHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
